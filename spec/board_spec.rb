c4959b24c53461afec1080ea5efde83fccc93f27
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-16 15:05:47 +0000

    board initializer

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,15 +3,22 @@ require 'cell'
 require 'board'
 
 describe 'board' do
-	
-let(:board) {Board.new}	
-let(:cell) {Cell.new}
+
+
+let(:cell) {Cell.new}	
+let(:board) {Board.new(Cell)}	
+let(:fake_ship){double(:ghost_ship) }
 
 	it 'should have a board' do
 		
 		expect(board.table).not_to eq(nil) 
 
 	end
+
+
+  it 'should load a ship to the board' do
+
+  end
 
 
 end
