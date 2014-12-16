fb41405a4961f0fca9a8413f8e6b75a7420852fe
jjlakin <j.lakin@gmail.com>
2014-12-16 15:29:22 +0000

    adding location array

--- lib/board.rb
+++ lib/board.rb
@@ -5,11 +5,26 @@ class Board
 
 	
 	def initialize(cell, size: size)
-		@table = {}
-		(1..size).each { |c| @table[c] = Array.new(size) {cell} }
+		@table = []
+		(0..size-1).each { |c| @table[c] = Array.new(size) {cell} }
 	end
 
-  def load_ship(ship: ship)
+  def load_ship(ship: ship, location)
+
+	  x1 = location[1]
+	  y1 = location[2]
+		x2 = location[3]
+		y2 = loaction[4]
+
+		if x1 == x2
+	  	table[][]
+		elsif y1 == y2
+			table[][]
+		else
+			raise "stupid coordinates"
+		end
+
+
   end  
 	
 
