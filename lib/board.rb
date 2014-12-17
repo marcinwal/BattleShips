2cac31367d1813ef41412d4bdfcd24a86683e2e5
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-17 09:37:44 +0000

    lenght check

--- lib/board.rb
+++ lib/board.rb
@@ -21,8 +21,10 @@ class Board
 		y2 = location[3]
 # byebug
 		if x1 == x2
+			 raise "coordinates do not match a ship lenght" (y2 - y1) != ship.length
 	  	(y1..y2).each {|y| @table[x1][y].load(ship) }
 		elsif y1 == y2
+			raise "coordinates do not match a ship lenght" (x2 - x1) != ship.length
 			(x1..x2).each {|x| @table[x][y1].load(ship) }
 		else
 			raise "stupid coordinates"
