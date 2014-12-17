008e1eaa2bc157317fc48f89cb9cc8f703415e32
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-17 16:11:37 +0000

    player update

--- lib/player.rb
+++ lib/player.rb
@@ -23,6 +23,7 @@ class Player
     l =  /[a..j]/i.match(string)
     n =  /\d+/.match(string)
     #to raise errors if not correct
+    raise "wrong coordinates" if (l == nil) or (n == nil)
     return l[0].upcase.ord - 'A'.ord, n[0].to_i - 1
   end  
 
